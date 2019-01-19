0c4f192d34728a889ec28134543c01a198b3bc7a
bereketgadhanom <bereketgadhanom@gmail.com>
2019-01-19 23:56:12 +0100

    lagene

---
+++ lag.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html lang="nb">
+  <head>
+    <title> </title>
+  </head>
+  <body>
+    <?php
+    <select>
+      <option> Ajax </option>
+      <option> Manchester United </option>
+      <option> Rosenborg Fotballklubb </option>
+      <option> Barcelona FC </option>
+      <option> Rosenborg </option>
+      <option> Barcelona FC </option>
+    </select>
+    ?>
+    <footer>
+    </footer>
+  </body>
+</html>
