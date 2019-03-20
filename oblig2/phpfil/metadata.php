1c57fc1e8429172c0ffa20c8ed35f6dac1242b15
bereketgadhanom <bereketgadhanom@gmail.com>
2019-03-20 18:03:16 +0100

    oblig-2

--- oblig2/phpfil/metadata.php
+++ oblig2/phpfil/metadata.php
@@ -1,6 +1,6 @@
 
     <!-- metadata for Facebook og Google -->
-        <meta property="og:image"       content="../bilder/metadata.jpg" />
+        <meta property="og:image"       content="https://itstud.hiof.no/~bereketa/uin/oblig2/bilder/metadata.jpg" />
         <meta property="og:title"       content="Film Arkiv"    />
         <meta propert="og:url"          content="index.php"     />
         <meta property="og:locale"      content="nb_NO"         />
